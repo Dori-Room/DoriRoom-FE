257981b6aefe2baffa72b990b15eaf1999ed5144
suwith <suwith@kookmin.ac.kr>
2025-06-25 05:16:49 +0900

    chore: 웹앱 빌드 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',        // 정적 SPA로 빌드
+    trailingSlash: true      // 라우팅 깨짐 방지
+}
 
 export default nextConfig;
